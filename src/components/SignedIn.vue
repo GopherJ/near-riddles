938cce19d13a5257d82f715f69bd2bbae5c203f2
Cheng JIANG <alex_cj96@foxmail.com>
2021-03-12 12:12:30 +0800

    WIP: frontend

--- src/components/SignedIn.vue
+++ src/components/SignedIn.vue
@@ -3,112 +3,17 @@
     <button class="link" style="float: right" v-on:click="logout">Sign out</button>
     <main>
       <h1>
-        <label
-          for="greeting"
-          style="color: var(--secondary);border-bottom: 2px solid var(--secondary);"
-        >{{ savedGreeting }}</label>
-        {{ accountId }}
+        Signed as: {{ accountId }}
       </h1>
-      <form v-on:submit.prevent="saveGreeting">
-        <fieldset ref="fieldset">
-          <label
-            for="greeting"
-            style="display:block; color:var(--gray);margin-bottom:0.5em;"
-          >Change greeting</label>
-          <div style="display:flex">
-            <input v-model="newGreeting" autocomplete="off" id="greeting" style="flex:1" />
-            <button id="save" style="border-radius:0 5px 5px 0">Save</button>
-          </div>
-        </fieldset>
-      </form>
-      <p>Look at that! A Hello World app! This greeting is stored on the NEAR blockchain. Check it out:</p>
-      <ol>
-        <li>
-          Look in
-          <code>src/App.vue</code> and
-          <code>src/utils.js</code>
-          - you'll see
-          <code>get_greeting</code>
-          and
-          <code>set_greeting</code> being called on
-          <code>contract</code>. What's this?
-        </li>
-        <li>
-          Ultimately, this
-          <code>contract</code> code is defined in
-          <code>assembly/main.ts</code>
-          - this is the source code for your
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://docs.near.org/docs/roles/developer/contracts/intro"
-          >smart contract</a>.
-        </li>
-        <li>
-          When you run
-          <code>npm run dev</code> or
-          <code>yarn dev</code>, the code in
-          <code>assembly/main.ts</code>
-          gets deployed to the NEAR testnet. You can see how this happens by looking in
-          <code>package.json</code>
-          at the
-          <code>scripts</code> section to find the
-          <code>dev</code> command.
-        </li>
-      </ol>
-      <hr />
-      <p>
-        To keep learning, check out
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://docs.near.org"
-        >the NEAR docs</a> or look through some
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://examples.near.org"
-        >example apps</a>.
-      </p>
     </main>
-
-    <Notification
-      v-show="notificationVisible"
-      ref="notification"
-      :networkId="networkId"
-      :msg="'called method: set_greeting'"
-      :contractId="contractId"
-      :visible="false"
-    />
   </div>
 </template>
 
 <script>
 import { logout } from "../utils"
 
-import Notification from "./Notification.vue"
-
 export default {
   name: "SignedIn",
-
-  beforeMount() {
-    if (this.isSignedIn) {
-      this.retrieveSavedGreeting()
-    }
-  },
-
-  components: {
-    Notification,
-  },
-
-  data: function () {
-    return {
-      savedGreeting: "",
-      newGreeting: "",
-      notificationVisible: false,
-    }
-  },
-
   computed: {
     isSignedIn() {
       return window.walletConnection? window.walletConnection.isSignedIn(): false
@@ -125,6 +30,7 @@ export default {
   },
 
   methods: {
+<<<<<<< Updated upstream
     retrieveSavedGreeting() {
       //retrieve greeting
       window.contract
@@ -174,6 +80,9 @@ export default {
     },
 
     logout: logout,
+=======
+    logout,
+>>>>>>> Stashed changes
   },
 }
 </script>
